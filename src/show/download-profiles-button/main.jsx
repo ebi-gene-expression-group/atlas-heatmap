8fc8c25035da03bc538ed052b13842a52c397d5b
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-01-18 12:52:12 +0000

    Use a Blob to download table content, a better solution for #137790855 [Fixes #137790855]

--- src/show/download-profiles-button/main.jsx
+++ src/show/download-profiles-button/main.jsx
@@ -1,11 +1,11 @@
-const React = require(`react`);
-const Modal = require(`react-bootstrap/lib/Modal`);
-const Button = require(`react-bootstrap/lib/Button`);
-const Glyphicon = require(`react-bootstrap/lib/Glyphicon`);
-const Disclaimers = require(`./Disclaimers.jsx`);
+const React = require('react');
+const Modal = require('react-bootstrap/lib/Modal');
+const Button = require('react-bootstrap/lib/Button');
+const Glyphicon = require('react-bootstrap/lib/Glyphicon');
+const Disclaimers = require('./Disclaimers.jsx');
 const PropTypes = require('../../PropTypes.js');
 
-const Download = require(`./Download.js`).commenceDownload;
+const Download = require('./Download.js').CommenceDownload;
 const DownloadProfilesButton = React.createClass({
     propTypes: {
         download: React.PropTypes.shape({
