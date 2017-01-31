fa6e67d33aef9a81b8bd46612a79ef128883187e
wbazant <wbazant@ebi.ac.uk>
2017-01-31 15:21:38 +0000

    Better documentation of our API

--- src/highchartsHeatmapRenderer.js
+++ src/highchartsHeatmapRenderer.js
@@ -5,23 +5,54 @@ const EventEmitter = require('events');
 const HighchartsHeatmapContainer = require('./HighchartsHeatmapContainer.jsx');
 
 /**
- * @param {Object}      options
- * @param {string}          options.proxyPrefix - optionally set as "http(s?)://" or to proxy URL
- * @param {string}          options.selfHosted - Set this as true if you want to host our content yourself, but have outwards links pointing to our original site
- * @param {boolean=}        options.disableGoogleAnalytics - Disable Google Analytics
- * @param {string=}         options.atlasHost - Atlas host with protocol and port
- * @param {string}          options.sourceURL - Where to source the data from
- * *                        e.g. /json/experiments/E-PROT-1, /json/genes/ENSG00000005801, /json/genesets/GO:0000001 or a widget endpoint
- * @param {string}          options.params - Alternate way of sourcing data if you do not provide the sourceURL
- * @param {string | Object} options.target - a <div> id or a DOM element, as returned by ReactDOM.findDOMNode()
- * @param {function}        options.fail - Callback to run if the AJAX request to the server fails. (jqXHR, textStatus)
- * @param {Object}          options.anatomogramDataEventEmitter - emits events to the facets tree to signal the existence of anatomogram
- * @param {boolean=}        options.showAnatomogram - optionally hide the anatomogram
- * @param {boolean}         options.isDifferential
- * @param {boolean}         options.isMultiExperiment
- * @param {boolean=}        options.isWidget
- * @param {string}          options.pathToFolderWithBundledResources - use if you're serving the /svg's from an unusual location. Development only.
- */
+ API of this widget
+------------------
+### Minimal example
+{
+  params:"geneQuery=zinc finger&species=mus musculus",
+  target:"myDomElementId"
+}
+
+### Note
+changes to this API will be announced to the mailing list atlas-widget@ebi.ac.uk
+
+### Target
+@param {string | Object} options.target - a <div> id or a DOM element, as returned by ReactDOM.findDOMNode()
+
+### Query settings
+@param {string}          options.params - Alternate way of sourcing data if you do not provide the sourceURL
+e.g. 'geneQuery=[{value:"BRCA1"}, {value:"REG1B"}]&species=homo sapiens',
+e.g. 'geneQuery=[{value:"PIP",category:"symbol"}]&species=homo sapiens',
+e.g. 'geneQuery=zinc finger&species=mus musculus',
+e.g. 'experiment=E-MTAB-513'
+
+### Optional recommended alternative query settings
+@param {string}          options.sourceURL - Where to source the data from (used by Atlas instead of options.params)
+e.g. https://www.ebi.ac.uk/gxa/json/experiments/E-PROT-1
+e.g. https://www.ebi.ac.uk/gxa/json/search/baselineResults?query={value:"zinc finger"}&species=homo sapiens
+e.g. https://www.ebi.ac.uk/gxa/widgets/heatmap/baselineAnalytics?geneQuery={value:"zinc finger"}&species=homo sapiens
+e.g. https://www.ebi.ac.uk/gxa/widgets/heatmap/referenceExperiment?experiment=E-MTAB-513
+Note: the URL is otherwise inferred from options.params.
+You could select this option and add yourself an integration test that checks what data the backend will serve you.
+
+### Optional connection setup
+@param {string}          options.proxyPrefix - proxy prefix for outbound links, optionally set as "http(s?)://" or to proxy URL e.g. "www.myproxy.org?url="
+@param {string}         options.atlasHost - Atlas host with protocol and port - where to source the data from, default: "https://www.ebi.ac.uk"
+
+### Optional options
+@param {boolean}        options.disableGoogleAnalytics - Disable Google Analytics
+@param {function}        options.fail - Callback to run if the AJAX request to the server fails. (jqXHR, textStatus)
+@param {boolean}        options.showAnatomogram - optionally hide the anatomogram
+
+### Advanced options
+@param {Object}          options.anatomogramDataEventEmitter - emits events to the facets tree to signal the existence of anatomogram
+@param {boolean}         options.isDifferential
+@param {boolean}         options.isMultiExperiment
+@param {boolean}        options.isWidget
+
+### Development only
+@param {string}          options.pathToFolderWithBundledResources - use if you're serving the /svg's from an unusual location. Development only.
+**/
 
 exports.render = function(options) {
 
