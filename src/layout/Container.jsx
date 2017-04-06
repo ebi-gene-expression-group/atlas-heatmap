91d64c129217fa11d75a29896d0d322aeb6a85cf
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-04-06 12:35:50 +0100

    Reuse propTypes from ChartWithAnatomogram; consistently indent code

--- src/layout/Container.jsx
+++ src/layout/Container.jsx
@@ -11,7 +11,34 @@ import ChartContainer from '../manipulate/ChartContainer.jsx'
 import DataPropTypes from './jsonPayloadPropTypes.js'
 import loadChartData from '../load/main.js'
 
-const CommonPropTypes = {
+const ChartWithAnatomogram = ({data, inProxy, outProxy, atlasUrl, showAnatomogram,isWidget}) => {
+    const {experiment, columnHeaders, columnGroupings, profiles, coexpressions, config, anatomogram} = data;
+    const pathToResources = inProxy + URI(`resources/js-bundles/`).absoluteTo(atlasUrl);
+
+    const chartData = loadChartData(data, inProxy, outProxy, atlasUrl, pathToResources, isWidget);
+
+    if (anatomogram && showAnatomogram) {
+        const Wrapped = Anatomogram.wrapComponent({
+            anatomogramData: anatomogram,
+            pathToResources: inProxy + URI(`resources/js-bundles/`).absoluteTo(atlasUrl),
+            expressedTissueColour: experiment ? `gray` : `red`,
+            hoveredTissueColour: experiment ? `red` : `purple`,
+            idsExpressedInExperiment: columnHeaders.map(header => header.factorValueOntologyTermId)
+        }, ChartContainer, {chartData})
+        return (
+            <Wrapped/>
+        )
+    } else {
+        return (
+            <ChartContainer {...{chartData}}
+                            ontologyIdsToHighlight={[]}
+                            onOntologyIdIsUnderFocus={() => {}}
+            />
+        )
+    }
+};
+
+ChartWithAnatomogram.propTypes = {
     inProxy: React.PropTypes.string.isRequired,
     outProxy: React.PropTypes.string.isRequired,
     atlasUrl: React.PropTypes.string.isRequired,
@@ -21,62 +48,34 @@ const CommonPropTypes = {
     data: DataPropTypes.isRequired
 };
 
-const ChartWithAnatomogram = ({data, inProxy, outProxy, atlasUrl, showAnatomogram,isWidget}) => {
-  const {experiment, columnHeaders, columnGroupings, profiles, coexpressions, config, anatomogram} = data;
-  const pathToResources = inProxy + URI(`resources/js-bundles/`).absoluteTo(atlasUrl);
-
-  const chartData = loadChartData(data, inProxy, outProxy, atlasUrl, pathToResources, isWidget);
-
-  if (anatomogram && showAnatomogram) {
-    const Wrapped = Anatomogram.wrapComponent({
-        anatomogramData: anatomogram,
-        pathToResources: inProxy + URI(`resources/js-bundles/`).absoluteTo(atlasUrl),
-        expressedTissueColour: experiment ? `gray` : `red`,
-        hoveredTissueColour: experiment ? `red` : `purple`,
-        idsExpressedInExperiment: columnHeaders.map(header => header.factorValueOntologyTermId)
-    }, ChartContainer, {chartData})
-    return (
-      <Wrapped/>
-    )
-  } else {
-    return (
-      <ChartContainer {...{chartData}}
-                      ontologyIdsToHighlight={[]}
-                      onOntologyIdIsUnderFocus={() => {}}
-      />
-    )
-  }
-};
-
-ChartWithAnatomogram.propTypes = CommonPropTypes;
 
 const Container = (props) => {
-  const {data, inProxy, outProxy, atlasUrl, showAnatomogram,isWidget} = props;
-  const {geneQuery, conditionQuery, species} = data.config;
+    const {data, inProxy, outProxy, atlasUrl, showAnatomogram,isWidget} = props;
+    const {geneQuery, conditionQuery, species} = data.config;
 
-  const moreInformationUrl = data.experiment ?    // single experiment?
-      URI(data.experiment.relUrl).absoluteTo(atlasUrl).search(``) :
-      URI(atlasUrl).segment(`query`).search({geneQuery, conditionQuery, species});
+    const moreInformationUrl = data.experiment ?    // single experiment?
+        URI(data.experiment.relUrl).absoluteTo(atlasUrl).search(``) :
+        URI(atlasUrl).segment(`query`).search({geneQuery, conditionQuery, species});
 
-  return (
-      <div>
-          {isWidget && data.experiment &&
-              <ExperimentDescription outProxy={outProxy}
-                                     experimentUrl={URI(data.experiment.relUrl).absoluteTo(atlasUrl).toString()}
-                                     description={data.experiment.description} />
-          }
+    return (
+        <div>
+            {isWidget && data.experiment &&
+            <ExperimentDescription outProxy={outProxy}
+                                   experimentUrl={URI(data.experiment.relUrl).absoluteTo(atlasUrl).toString()}
+                                   description={data.experiment.description} />
+            }
 
-          <ChartWithAnatomogram {...props} />
+            <ChartWithAnatomogram {...props} />
 
-          {isWidget &&
-              <Footer outProxy={outProxy}
-                      atlasUrl={atlasUrl}
-                      moreInformationUrl={moreInformationUrl.toString()} />
-          }
-      </div>
-  )
+            {isWidget &&
+            <Footer outProxy={outProxy}
+                    atlasUrl={atlasUrl}
+                    moreInformationUrl={moreInformationUrl.toString()} />
+            }
+        </div>
+    )
 };
 
-Container.propTypes = CommonPropTypes;
+Container.propTypes = ChartWithAnatomogram.propTypes;
 
 export default Container
