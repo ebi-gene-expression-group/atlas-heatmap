de2864d11bcc68c6ce43ae4585593e92b108c9b1
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-04-10 10:42:12 +0100

    Replace numberWithCommas with a better version that doesn’t put commas whenever there are more than two decimal places

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,10 @@
 const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.substr(1);
-const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+// http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+const numberWithCommas = x => {
+    const parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+};
 
 export {capitalizeFirstLetter, numberWithCommas};
