6882db98924769a9e4a6aef4999b72b82be1e3f2
wbazant <wbazant@ebi.ac.uk>
2017-05-05 14:50:42 +0100

    Do not offer to show boxplots when we have no boxplots to show

--- src/load/boxplotData.js
+++ src/load/boxplotData.js
@@ -17,12 +17,21 @@ const getXAxisCategories = columnHeaders =>
 const getTitle = profiles =>
     profiles.rows[0].name + ` – ` + profiles.rows[0].id;
 
+const tryCreateBoxplotData = (profiles, columnHeaders, experiment) => {
+  const dataSeries = getDataSeries(profiles)
+  if(dataSeries.map((e)=>e.length).reduce((l,r)=>l+r, 0)){
+    return {
+        dataSeries,
+        xAxisCategories: getXAxisCategories(columnHeaders),
+        title: getTitle(profiles),
+        unit: getUnits(experiment)
+    }
+  } else {
+    return null
+  }
+}
+
 export default (profiles, columnHeaders, experiment) =>
-    isBaseline(experiment) && profiles.rows.length === 1 ?
-        ({
-            dataSeries: getDataSeries(profiles),
-            xAxisCategories: getXAxisCategories(columnHeaders),
-            title: getTitle(profiles),
-            unit: getUnits(experiment)
-        }) :
-    null;
+    isBaseline(experiment) && profiles.rows.length === 1
+    ? tryCreateBoxplotData(profiles,columnHeaders, experiment)
+    : null
