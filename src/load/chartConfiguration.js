e433fc0feee73e33c2340dd7e8459c324e3f9038
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-16 03:40:52 +0100

    URL for Genome Browser must be absolute and is correctly built

--- src/load/chartConfiguration.js
+++ src/load/chartConfiguration.js
@@ -15,9 +15,8 @@ const introductoryMessage = (experiment, profiles) => {
 // _x_ and _y_ are placeholders to be replaced when clicking on a heatmap cell (see HeatmapCanvas.jsx)
 // TODO User URI.js to build URL (?)
 const genomeBrowserPath = (experiment, atlasUrl) => {
-    const fullyQualifiedAtlasUrl = atlasUrl.startsWith(`http`) ? atlasUrl :
-      URI(atlasUrl, `${window.location.protocol}//${window.location.hostname}`).toString();
-    const trackFileUrl = `${fullyQualifiedAtlasUrl}/experiments/${experiment.accession}/tracks/${experiment.accession}._x_`;
+    const fullyQualifiedAtlasUrl = atlasUrl.startsWith(`http`) ? atlasUrl : URI(atlasUrl, `${window.location.protocol}//${window.location.hostname}`).toString();
+    const trackFileUrl = URI(`experiments/${experiment.accession}/tracks/${experiment.accession}._x_`, fullyQualifiedAtlasUrl).toString();
 
     const contigViewBottom =
         `contigviewbottom=url:${trackFileUrl}.genes.${isDifferential ? `log2foldchange` : `expressions`}.bedGraph`;
