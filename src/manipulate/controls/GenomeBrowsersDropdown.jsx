2817c5e53d2bdfb678aba588b610f783eca6a374
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-05 15:41:36 +0100

    Use eventKey to nicely separate genome browser ID/URL string from its label on the button

--- src/manipulate/controls/GenomeBrowsersDropdown.jsx
+++ src/manipulate/controls/GenomeBrowsersDropdown.jsx
@@ -3,16 +3,19 @@ import Dropdown from 'react-bootstrap/lib/Dropdown';
 import MenuItem from 'react-bootstrap/lib/MenuItem';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
-class GenomeBrowsersDropdown extends React.Component{
+class GenomeBrowsersDropdown extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
     handleChange(eventKey, event) {
         event.preventDefault();
-        this.props.onSelect(event.target.text);
+        this.props.onSelect(eventKey);
     }
 
     _genomeBrowserIcon(genomeBrowser) {
         switch (genomeBrowser) {
-            case `No genome browser selected`:
+            case `none`:
                 return `eye-close`;
             default:
                 return `eye-open`;
@@ -20,6 +23,18 @@ class GenomeBrowsersDropdown extends React.Component{
     }
 
     render() {
+        const genomeBrowsers =
+            [
+                {
+                    id: `none`,
+                    label: `No genome browser selected`
+                },
+                ...this.props.genomeBrowsers.map(genomeBrowserName => ({
+                    id: genomeBrowserName.replace(/\s+/g, ``).toLowerCase(),
+                    label: `${genomeBrowserName} genome browser`
+                }))
+            ];
+
         return (
             <div>
                 <Dropdown id="genome-browsers-dropdown"
@@ -28,13 +43,14 @@ class GenomeBrowsersDropdown extends React.Component{
 
                     <Dropdown.Toggle bsSize="small"
                                      style={{textTransform: `unset`, letterSpacing: `unset`, height: `unset`}}>
-                        <Glyphicon glyph={this._genomeBrowserIcon(this.props.selected)} /> {this.props.selected}
+                        <Glyphicon glyph={this._genomeBrowserIcon(this.props.selected)}
+                        /> {genomeBrowsers.find(gb => this.props.selected === gb.id).label}
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu bsSize="small">
-                        {this.props.genomeBrowsers.map(genomeBrowser =>
-                            <MenuItem style={{listStyleImage: `none`}} key={genomeBrowser} href="#">
-                                {genomeBrowser}
+                        {genomeBrowsers.map(gb =>
+                            <MenuItem style={{listStyleImage: `none`}} key={gb.id} eventKey={gb.id} href="#">
+                                {gb.label}
                             </MenuItem>
                         )}
                     </Dropdown.Menu>
@@ -48,7 +64,7 @@ class GenomeBrowsersDropdown extends React.Component{
 GenomeBrowsersDropdown.propTypes = {
     genomeBrowsers: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
     selected: React.PropTypes.string,
-    onSelect: React.PropTypes.func,
+    onSelect: React.PropTypes.func
 };
 
 export default GenomeBrowsersDropdown;
