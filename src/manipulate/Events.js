26644c6d0f6d93010666b792ad788bc70d6e08d3
wbazant <wbazant@ebi.ac.uk>
2017-06-06 17:47:45 +0100

    Add a todo with an idea for future developers of Atlas

--- src/manipulate/Events.js
+++ src/manipulate/Events.js
@@ -34,7 +34,17 @@ const makeEventCallbacks = ({heatmapData, onSelectOntologyIds, genomeBrowser, ex
       onSelectOntologyIds([])
     },
 
-    // For this to work genomeBrowser needs to be included in the props that cause a re-render in HeatmapCanvas.jsx
+    /*
+      For this to work genomeBrowser needs to be included in the props that cause a re-render in HeatmapCanvas.jsx
+
+      TODO we have suffered a bit of a defeat here because it made us include accessKey.
+      If instead of genomeBrowser string we had a {name, uri} here, we could do
+      URI(genomeBrowser.uri, atlasUrl).search({
+        experimentAccession: experimentAccession,
+        geneId: heatmapData.xAxisCategories[x].info.trackId,
+        trackId: heatmapData.yAxisCategories[y].info.trackId
+      })
+    */
     onClick: genomeBrowser !== `none` ?
       (x, y) => {
         window.open(URI(`external-services/genome-browser/${genomeBrowser}`, atlasUrl).search({
