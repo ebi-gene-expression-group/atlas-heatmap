c7fb5c2d7dcbaf9df986b5a2d3009d0d493a4141
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-08-14 23:13:04 +0100

    Document new option; also previous commit was: remove default options twice

--- src/Main.js
+++ src/Main.js
@@ -17,6 +17,7 @@ import ContainerLoader from './layout/ContainerLoader.js'
  * @param {string}          options.atlasUrl - Atlas host and path with protocol and port
  * @param {string}          options.inProxy - Inbound proxy to pull assets from outside your domain
  * @param {string}          options.outProxy - Outbound proxy for links that take you outside the current domain
+ * @param {boolean}         options.useEbiFramework - Import styles from EBI Framework 1.2 lite
  * @param {string}          options.experiment
  * @param {Object|string}   options.query - Query object or relative URL endpoint to source data from:
  *                              e.g. json/experiments/E-PROT-1, /json/genes/ENSG00000005801, /json/genesets/GO:0000001
