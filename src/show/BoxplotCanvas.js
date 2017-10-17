ba83eadd2b17808dad2dfc6739b6c4c21034ac2f
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-17 16:56:42 +0100

    Make boxplot Y axis logarithmic

--- src/show/BoxplotCanvas.js
+++ src/show/BoxplotCanvas.js
@@ -4,6 +4,38 @@ import ReactHighcharts from 'react-highcharts'
 import HighchartsMore from 'highcharts/highcharts-more'
 HighchartsMore(ReactHighcharts.Highcharts)
 
+// Taken from http://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/highcharts/yaxis/type-log-negative/
+const allowNegativeLog = (H) => {
+  H.Axis.prototype.allowNegativeLog = true;
+
+  // Override conversions
+  H.Axis.prototype.log2lin = (num) => {
+    const isNegative = num < 0
+    let adjustedNum = Math.abs(num)
+
+    if (adjustedNum < 10) {
+        adjustedNum += (10 - adjustedNum) / 10
+    }
+
+    const result = Math.log(adjustedNum) / Math.LN10
+    return isNegative ? -result : result
+  }
+
+  H.Axis.prototype.lin2log = (num) => {
+    const isNegative = num < 0
+    const absNum = Math.abs(num)
+    let result = Math.pow(10, absNum)
+
+    if (result < 10) {
+        result = (10 * (result - 1)) / (10 - 1)
+    }
+
+    return isNegative ? -result : result
+  }
+}
+
+allowNegativeLog(ReactHighcharts.Highcharts)
+
 const BoxplotCanvas = ({title, xAxisCategories, dataSeries, unit}) => {
 
     const initialMarginRight = 60
@@ -78,6 +110,7 @@ const BoxplotCanvas = ({title, xAxisCategories, dataSeries, unit}) => {
         },
 
         yAxis: {
+            type: `logarithmic`,
             title: {
                 text: `Expression` + (unit ? ` (${unit})`: ``)
             },
