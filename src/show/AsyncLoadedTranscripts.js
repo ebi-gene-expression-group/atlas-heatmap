ec81727f53c7c6a04ef42eff4dff3822e3b22cfd
wbazant <wbazant@ebi.ac.uk>
2017-11-03 12:52:22 +0000

    Delete dead code

--- src/show/AsyncLoadedTranscripts.js
+++ src/show/AsyncLoadedTranscripts.js
@@ -14,7 +14,6 @@ const baseConfig = ({xAxisCategories}) => ({
 		zoomType: 'x',
 		events: {
 	      load: function() {
-
 			//works apart from when you later take some series out with the menu
 			//http://jsfiddle.net/sza4odkz/1/
 			this.series.forEach((series,ix,self) => {
@@ -210,71 +209,3 @@ export default connect(props => ({
         url: props.url
     },
 }))(QuietTranscriptsLoader)
-
-class Demo extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      loading: false,
-      data:{}
-    }
-
-    this._handleChangeExperiment = this._handleChangeExperiment.bind(this)
-    this._handleChangeGeneId = this._handleChangeGeneId.bind(this)
-    this._handleSubmit = this._handleSubmit.bind(this)
-  }
-
-  _handleChangeExperiment(event) {
-    this.setState({
-      experimentAccession: event.target.value
-    })
-  }
-  _handleChangeGeneId(event) {
-    this.setState({
-      geneId: event.target.value
-    })
-  }
-
-  _handleSubmit(event) {
-    event.preventDefault()
-    const url = ourUrl("https://www-test.ebi.ac.uk/gxa/", this.state.experimentAccession, this.state.geneId)
-
-    this.setState({
-      url: url,
-      loading:true,
-  }, fetchResponseJson.bind(this, url, (data) => this.setState({data:data, loading: false})))
-  }
-
-
-  render() {
-    return(
-      <div className={`row column`}>
-        <div className={`row column`}>
-          <form onSubmit={this._handleSubmit}>
-            <label>Experiment</label>
-            <input type={`text`} onChange={this._handleChangeExperiment} value={this.state.experimentAccession}/>
-            <label>Gene id</label>
-            <input type={`text`} onChange={this._handleChangeGeneId} value={this.state.geneId}/>
-            <input className={`button`} type="submit" value="Go!" />
-          </form>
-        </div>
-
-        { this.state.loading
-            ?  ""
-            : this.state.url
-                ? <div>
-                        <i>
-                        Reading from:
-                        {this.state.url}
-                        </i>
-                        <br/>
-                        <Main {...this.state.data}/>
-                    </div>
-                : ""
-
-        }
-      </div>
-    )
-  }
-}
