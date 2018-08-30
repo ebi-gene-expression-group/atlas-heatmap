2241345e76ab0a58f1787dc3cac789f7bfc6491a
Lingyun Zhao <33519183+lingyun1010@users.noreply.github.com>
2018-08-30 10:38:11 +0100

    Reword heatmap footer (#13)
    * Reword heatmap footer
    
    * Clean up footer code

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 const Footer = props =>
   <div style={{marginBottom: `2rem`, clear: `both`, width: `100%`}}>
-    <a href={props.outProxy + props.moreInformationUrl}> See more expression data at Expression Atlas.</a>
-    <br/>This expression view is provided by <a href={props.outProxy + props.atlasUrl}>Expression Atlas</a>.
+    This page is a summary of the data held in <a href={props.outProxy + props.atlasUrl}>Expression Atlas</a> for this gene; 
+    click <a href={props.outProxy + props.moreInformationUrl}>here</a> for the full record.
     <br/>Please send any queries or feedback to <a href="mailto:arrayexpress-atlas@ebi.ac.uk">arrayexpress-atlas@ebi.ac.uk</a>.
   </div>
 
