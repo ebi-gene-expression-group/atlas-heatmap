245a7e88de063ddbca9634183a683318b4524faf
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-08-31 02:18:23 +0100

    Suggest users to get in touch with the Atlas team using the EBI form

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types'
 
 const Footer = props =>
   <div style={{marginBottom: `2rem`, clear: `both`, width: `100%`}}>
-    This page is a summary of the data held in <a href={props.outProxy + props.atlasUrl}>Expression Atlas</a> for this gene; 
-    click <a href={props.outProxy + props.moreInformationUrl}>here</a> for the full record.
-    <br/>Please send any queries or feedback to <a href="mailto:arrayexpress-atlas@ebi.ac.uk">arrayexpress-atlas@ebi.ac.uk</a>.
+    This page is a summary of the data held in <a href={props.outProxy + props.atlasUrl}>Expression Atlas</a> for this
+    gene; click <a href={props.outProxy + props.moreInformationUrl}>here</a> for the full record.
+    <br/>Please send any queries or feedback via <a href="https://www.ebi.ac.uk/support/gxa">the EBI Support & feedback
+    form.</a>.
   </div>
 
 Footer.propTypes = {
